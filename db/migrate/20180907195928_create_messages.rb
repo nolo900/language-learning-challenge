273aeb9db2b1191ad2edfa808145097c8a804fe0
Austin Miles <abm@austinbmiles.com>
2018-09-07 16:19:56 -0400

    run migrations, adjust to handle messages

--- db/migrate/20180907195928_create_messages.rb
+++ db/migrate/20180907195928_create_messages.rb
@@ -1,12 +1,15 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.references :sender, foreign_key: true
-      t.references :recipient, foreign_key: true
+      t.references :sender
+      t.references :recipient
       t.references :language, foreign_key: true
       t.text :body
 
       t.timestamps
     end
+
+    add_foreign_key :messages, :users, column: :sender_id, primary_key: :id
+    add_foreign_key :messages, :users, column: :recipient_id, primary_key: :id
   end
 end
