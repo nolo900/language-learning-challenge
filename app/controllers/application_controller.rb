e96f0d753edb7a895e697eaa02a82b235a9c97b9
Austin Miles <abm@austinbmiles.com>
2018-09-09 16:42:04 -0400

    added simple error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, :with => :resource_not_found
+
+  def index
+    render :plain => "Welcome to Language Learning via Penpal!"
+  end
+
+  def error
+    render :plain => "Route not found."
+  end
+
+  def resource_not_found
+    render :plain => "Resource not found."
+  end
+
 end
